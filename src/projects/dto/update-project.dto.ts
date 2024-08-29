c4addbd4a621f9f99bce0c2d436d16da97144b77
Евгений Горин <evg_gorin@mail.ru>
2024-08-29 23:45:21 +0400

    Update dto

--- src/projects/dto/update-project.dto.ts
+++ src/projects/dto/update-project.dto.ts
@@ -1,17 +1,4 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsOptional, IsString, MaxLength } from "class-validator";
+import { PartialType } from "@nestjs/swagger";
+import { CreateProjectDto } from "./create-project.dto";
 
-export class UpdateProjectDto {
-
-  @ApiProperty({ example: 'Project title', description: 'Название проекта' })
-  @IsString({ message: 'Должно быть строкой' })
-  @IsOptional()
-  @MaxLength(50, { message: 'Название проекта не должно превышать 50 символов' })
-  readonly title?: string;
-
-  @ApiProperty({ example: 'Project description', description: 'Название проекта' })
-  @IsString({ message: 'Должно быть строкой' })
-  @IsOptional()
-  @MaxLength(255, { message: 'Описание проекта не должно превышать 255 символов' })
-  readonly description?: string;
-}
+export class UpdateProjectDto extends PartialType(CreateProjectDto) {}
--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,17 +1,4 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsOptional, IsString, MaxLength } from "class-validator";
+import { PartialType } from "@nestjs/swagger";
+import { CreateTaskDto } from "./create-task.dto";
 
-export class UpdateTaskDto {
-
-  @ApiProperty({ example: 'Task title', description: 'Название задачи' })
-  @IsString({ message: 'Должно быть строкой' })
-  @IsOptional()
-  @MaxLength(50, { message: 'Название задачи не должно превышать 50 символов' })
-  readonly title?: string;
-
-  @ApiProperty({ example: 'Task description', description: 'Описание задачи' })
-  @IsString({ message: 'Должно быть строкой' })
-  @IsOptional()
-  @MaxLength(255, { message: 'Описание задачи не должно превышать 255 символов' })
-  readonly description?: string;
-}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
