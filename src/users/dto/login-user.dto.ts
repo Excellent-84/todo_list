65bc8e90fbfd73429f84e94d2024a75ee704fc9d
Евгений Горин <evg_gorin@mail.ru>
2024-08-30 00:11:00 +0400

    Update dto login

--- src/users/dto/login-user.dto.ts
+++ src/users/dto/login-user.dto.ts
@@ -1,20 +1,9 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsString, Length } from "class-validator";
+import { ApiProperty, OmitType } from "@nestjs/swagger";
+import { CreateUserDto } from "./create-user.dto";
 
 export class TokenResponse {
   @ApiProperty({ example: 'eyJhbGciOiJIUzI1NiIsInR5c...', description: 'Токен' })
   token: string;
 }
 
-export class LoginUserDto {
-
-  @ApiProperty({ example: 'john@example.com', description: 'Почтовый адрес' })
-  @IsEmail({}, { message: 'Некорректный email' })
-  @IsString({ message: 'Должно быть строкой' })
-  readonly email: string;
-
-  @ApiProperty({ example: '12345678', description: 'Пароль' })
-  @IsString({ message: 'Должно быть строкой' })
-  @Length(8, 16, { message: 'Не меньше 8 и не больше 16 символов' })
-  readonly password: string;
-}
+export class LoginUserDto extends OmitType(CreateUserDto, ['name'] as const) {}
